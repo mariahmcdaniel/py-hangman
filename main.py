280cc69c6be7aeaaa311dd2fdf69aca273d0833e
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2024-02-12 21:55:24 -0500

    choose word from pool - logic started

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 import hangman_words
+import random
 
-category = int(input("choose a category!\n\n1:Food & Beverage \n2:Animals"))
+while True:
+    try:
+        category = int(input("\n\n                Choose A Category!\n\n1:Food & Beverage \n2:Animals\n\n"))
+        if category == 1 or category == 2:
+            break
+    except ValueError:
+        print("\n\n\nInvalid Input. Try again")
+    else:
+        print("\n           Please enter either 1 or 2!")
 
-if int(category) == 1:
+if category == 1:
     word_pool = hangman_words.food_pool
+    print("Great, you chose Food and Beverage")
+elif category == 2:
+    word_pool = hangman_words.animal_pool
+    print("Great, you chose Animals")
+
+word_index = len(word_pool)
